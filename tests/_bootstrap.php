69e74a5319787c3437c59947970b0968faf32e05
lucor <lu.corbo@gmail.com>
2014-02-18 12:20:39 +0100

    Fixed php 5.3 support: Removed AspectMock init

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -3,9 +3,10 @@
 include __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/start.php';
 
-$kernel = \AspectMock\Kernel::getInstance();
-$kernel->init(array(
-    'debug' => true,
-    //'includePaths' => [__DIR__.'/../src']
-    'includePaths' => array(__DIR__.'/../app/models')
-));
+//Remove comments to add AspectMock support. Requires PHP >= 5.4
+//$kernel = \AspectMock\Kernel::getInstance();
+//$kernel->init(array(
+//    'debug' => true,
+//    //'includePaths' => [__DIR__.'/../src']
+//    'includePaths' => array(__DIR__.'/../app/models')
+//));
